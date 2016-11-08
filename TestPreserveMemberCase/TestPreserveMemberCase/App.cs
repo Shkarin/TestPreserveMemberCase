4599456a461a1662509b9e723c1189d0561b619e
shkarin_evgeniy <shkarin_evgeniy@RG103>
2016-11-08 14:12:07 +0500

    Test

---
+++ TestPreserveMemberCase/TestPreserveMemberCase/App.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using Bridge;
+using Bridge.Html5;
+
+namespace TestPreserveMemberCase
+{
+	public class App
+	{
+		public class Struct
+		{
+			public override string ToString()
+			{
+				return "Struct";
+			}
+
+			public override object ValueOf()
+			{
+				return "ValueOf";
+			}
+
+			public override int GetHashCode()
+			{
+				return 1;
+			}
+		}
+
+		public static void Main()
+		{
+			object obj = new Struct();
+			var str = new Struct();
+			Console.WriteLine(obj.ToString());
+			Console.WriteLine(str.ToString());
+
+			Console.WriteLine(obj.ValueOf());
+			Console.WriteLine(str.ValueOf());
+
+			Console.WriteLine(obj.GetHashCode());
+			Console.WriteLine(str.GetHashCode());
+		}
+	}
+}
---
+++ TestPreserveMemberCase/TestPreserveMemberCase/Bridge/output/testPreserveMemberCase.js
@@ -0,0 +1,35 @@
+﻿/**
+ * @version 1.0.0.0
+ * @copyright Copyright ©  2016
+ * @compiler Bridge.NET 15.3.0
+ */
+Bridge.assembly("TestPreserveMemberCase", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("TestPreserveMemberCase.App", {
+        $main: function () {
+            var obj = new TestPreserveMemberCase.App.Struct();
+            var str = new TestPreserveMemberCase.App.Struct();
+            Bridge.Console.log(obj.toString());
+            Bridge.Console.log(str.ToString());
+
+            Bridge.Console.log(obj.valueOf());
+            Bridge.Console.log(str.ValueOf());
+
+            Bridge.Console.log(Bridge.getHashCode(obj));
+            Bridge.Console.log(str.GetHashCode());
+        }
+    });
+
+    Bridge.define("TestPreserveMemberCase.App.Struct", {
+        ToString: function () {
+            return "Struct";
+        },
+        ValueOf: function () {
+            return "ValueOf";
+        },
+        GetHashCode: function () {
+            return 1;
+        }
+    });
+});
